5f522b3c682257ec0d9671c36942cd5b99874dc9
Rafael Ferraz Barra <104329840+Rafael-Ferraz-Barra@users.noreply.github.com>
2024-09-25 17:42:40 -0300

    Add files via upload

--- Codigo/frontend/src/App.js
+++ Codigo/frontend/src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/Login';
 import Venda from './pages/Venda';
 import CadastrarCliente from './pages/CadastrarCliente';
 import Caixa from './pages/Caixa';
+import FechamentoVenda from './pages/FechamentoVenda';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/venda" element={<Venda />} />
           <Route path="/cadastrar-cliente" element={<CadastrarCliente />} />
           <Route path="/caixa" element={<Caixa />} />
+          <Route path="/fechamento-venda" element={<FechamentoVenda />} />
         </Routes>
       </div>
     </Router>
