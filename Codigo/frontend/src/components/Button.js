b53954614b47f60ef5f9e71fef5b988de3e4df08
Rafael-Ferraz-Barra <rafael.barra.1384417@sga.pucminas.br>
2024-11-05 12:13:38 -0300

    estilo da sidebar

--- Codigo/frontend/src/components/Button.js
+++ Codigo/frontend/src/components/Button.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const sizeClasses = {
-  giant: 'w-[240px] h-[249px] flex items-center justify-center rounded-3xl',
-  big: 'text-[40px] px-12 py-3 rounded-lg',
-  default: 'text-[15px] px-4 py-2 rounded-lg',
-  small: 'text-[13px] px-2 py-1 rounded-lg',
+  giant: "w-[216px] h-[224px] flex items-center justify-center rounded-3xl",
+  big: "text-[40px] px-12 py-3 rounded-lg",
+  default: "text-[15px] px-4 py-2 rounded-lg",
+  small: "text-[13px] px-2 py-1 rounded-lg",
 };
 
-const Button = ({ children, size = 'default', className = '', fullWidth = false, image, ...props }) => {
+const Button = ({
+  children,
+  size = "default",
+  className = "",
+  fullWidth = false,
+  image,
+  ...props
+}) => {
   const sizeClass = sizeClasses[size] || sizeClasses.default;
-  const fullWidthClass = fullWidth ? ' w-[calc(100%-32px)]' : '';
+  const fullWidthClass = fullWidth ? " w-[calc(100%-32px)]" : "";
 
   return (
     <button
       className={`bg-[#9b5c6f] hover:bg-[#8a4f61] text-white font-bold ${sizeClass} ${fullWidthClass} ${className}`}
       {...props}
     >
-      {size === 'giant' && image ? (
+      {size === "giant" && image ? (
         <div className="w-full h-full p-8">
-          <img src={image} alt="Botão" className="w-full h-full object-contain" />
+          <img
+            src={image}
+            alt="Botão"
+            className="w-full h-full object-contain"
+          />
         </div>
       ) : (
         children
--- Codigo/frontend/src/components/SideDrawer.js
+++ Codigo/frontend/src/components/SideDrawer.js
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import axios from 'axios';
-import { FaShoppingCart, FaChartBar, FaCashRegister, FaQuestionCircle, FaUsers } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+import {
+  FaShoppingCart,
+  FaChartBar,
+  FaCashRegister,
+  FaQuestionCircle,
+  FaUsers,
+} from "react-icons/fa";
 
 const SideDrawer = ({ isOpen }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -10,52 +16,67 @@ const SideDrawer = ({ isOpen }) => {
 
   const handleLogout = async () => {
     try {
-      await axios.get('http://localhost:3000/api/login/logout');
-      localStorage.removeItem('token');
+      await axios.get("http://localhost:3000/api/login/logout");
+      localStorage.removeItem("token");
     } catch (error) {
-      console.error('Erro ao fazer logout:', error);
+      console.error("Erro ao fazer logout:", error);
     }
   };
-
 
   return (
     <>
       <div
-        className={`fixed top-0 left-0 h-full ${isCollapsed && !isHovering ? 'w-16' : 'w-64'} 
+        className={`fixed top-0 left-0 h-full ${
+          isCollapsed && !isHovering ? "w-16" : "w-64"
+        } 
                     bg-[#f6b9b6] transform transition-width duration-150 ease-in-out z-50`}
         onMouseEnter={() => setIsHovering(true)}
         onMouseLeave={() => setIsHovering(false)}
-        style={{ transition: 'width 0.15s ease-in-out' }} // Adiciona animação de expansão
+        style={{ transition: "width 0.15s ease-in-out" }} // Adiciona animação de expansão
       >
-        <div className="flex flex-col h-full justify-between p-4">
-          <div className="flex flex-col gap-4">
+        <div className="flex flex-col h-full justify-between p-6">
+          <div className="flex flex-col gap-8">
             <Link to="/home" className="mb-4 text-[#7d4b5f]">
-              <h3>{isCollapsed && !isHovering ? 'F' : 'FILIGRANA'}</h3>
+              <h3>{isCollapsed && !isHovering ? "F" : "FILIGRANA"}</h3>
             </Link>
             <nav>
               <ul className="space-y-4">
                 <li>
-                  <Link to="/venda" className="flex items-center gap-3 hover:text-[#9b5c6f]">
+                  <Link
+                    to="/venda"
+                    className="flex items-center gap-3 hover:text-[#9b5c6f]"
+                  >
                     <FaShoppingCart />
                     {(isHovering || !isCollapsed) && <span>NOVA VENDA</span>}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/clientes" className="flex items-center gap-3 hover:text-[#9b5c6f]">
+                  <Link
+                    to="/clientes"
+                    className="flex items-center gap-3 hover:text-[#9b5c6f]"
+                  >
                     <FaUsers />
                     {(isHovering || !isCollapsed) && <span>CLIENTES</span>}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/fechamento-de-caixa" className="flex items-center gap-3 hover:text-[#9b5c6f]">
+                  <Link
+                    to="/fechamento-de-caixa"
+                    className="flex items-center gap-3 hover:text-[#9b5c6f]"
+                  >
                     <FaCashRegister />
                     {(isHovering || !isCollapsed) && <span>FECHAR CAIXA</span>}
                   </Link>
                 </li>
                 <li>
-                  <Link to="/painel-de-dados" className="flex items-center gap-3 hover:text-[#9b5c6f]">
+                  <Link
+                    to="/painel-de-dados"
+                    className="flex items-center gap-3 hover:text-[#9b5c6f]"
+                  >
                     <FaChartBar />
-                    {(isHovering || !isCollapsed) && <span>PAINEL DE DADOS</span>}
+                    {(isHovering || !isCollapsed) && (
+                      <span>PAINEL DE DADOS</span>
+                    )}
                   </Link>
                 </li>
               </ul>
@@ -64,13 +85,20 @@ const SideDrawer = ({ isOpen }) => {
           <div>
             <ul className="space-y-4 py-4">
               <li>
-                <Link to="/" className="flex items-center gap-3 hover:text-gray-700" onClick={handleLogout}>
+                <Link
+                  to="/"
+                  className="flex items-center gap-3 hover:text-gray-700"
+                  onClick={handleLogout}
+                >
                   <FaQuestionCircle />
                   {(isHovering || !isCollapsed) && <span>SAIR</span>}
                 </Link>
               </li>
               <li>
-                <Link to="/ajuda" className="flex items-center gap-3 hover:text-gray-700">
+                <Link
+                  to="/ajuda"
+                  className="flex items-center gap-3 hover:text-gray-700"
+                >
                   <FaQuestionCircle />
                   {(isHovering || !isCollapsed) && <span>AJUDA</span>}
                 </Link>
