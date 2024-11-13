707833d484f216960fd266d0d8ac548d7b0c841f
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2024-11-13 07:16:22 -0300

    Integra rota de painel de dados anual com API

--- Codigo/frontend/src/services/filigranaapi/dashboard.js
+++ Codigo/frontend/src/services/filigranaapi/dashboard.js
@@ -7,38 +7,5 @@ export async function getMonthlySummary(month) {
 }
 
 export async function getAnnualSummary(year) {
-  // return httpClientGet(`${ENDPOINT}/annual-summary/${year}`);
-
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-
-  const y = 2024;
-  return {
-    year: y.toString(),
-    totalSales: 2,
-    totalSalesAmount: 54.68,
-    salesByMonths: Array.from({ length: 12 }, (_, i) => ({
-      month: `${y}-${(i + 1).toString().padStart(2, "0")}`,
-      totalAmount: Math.floor(Math.random() * 1000),
-    })),
-    salesByType: [
-      {
-        saleType: "Varejo",
-        count: 1,
-      },
-      {
-        saleType: "Atacado",
-        count: 1,
-      },
-    ],
-    salesByPaymentMethod: [
-      {
-        paymentMethod: "Dinheiro",
-        count: 1,
-      },
-      {
-        paymentMethod: "Pix",
-        count: 1,
-      },
-    ],
-  };
+  return httpClientGet(`${ENDPOINT}/annual-summary/${year}`);
 }
