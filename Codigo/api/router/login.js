d5f6a762b1d0bb6ee562df70097266ebf8eac349
Rafael-Ferraz-Barra <rafael.barra.1384417@sga.pucminas.br>
2025-01-28 16:09:04 -0300

    senha alterada

--- Codigo/api/router/login.js
+++ Codigo/api/router/login.js
@@ -1,34 +1,32 @@
-const jwt = require('jsonwebtoken');
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
   const { usuario, senha } = req.body;
-  if (usuario === 'admin' && senha === 'admin') {
-    const token = jwt.sign({ usuario }, 'token_filigrana', { expiresIn: '2h' });
+  if (usuario === "1" && senha === "1") {
+    const token = jwt.sign({ usuario }, "token_filigrana", { expiresIn: "2h" });
     res.json({ token });
   } else {
-    res.status(401).json({ error: 'Usuário ou senha inválida!' });
+    res.status(401).json({ error: "Usuário ou senha inválida!" });
   }
-  });
+});
 
 router.post("/verify", (req, res) => {
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) return res.status(403).json({ error: 'Token não fornecido' });
-  const token = authHeader.split(' ')[1]; 
-  jwt.verify(token, 'token_filigrana', (err, decoded) => {
-    if (err) return res.status(500).json({ valid: 'false' });
+  const authHeader = req.headers["authorization"];
+  if (!authHeader)
+    return res.status(403).json({ error: "Token não fornecido" });
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, "token_filigrana", (err, decoded) => {
+    if (err) return res.status(500).json({ valid: "false" });
     req.usuario = decoded.usuario;
-    res.status(200).json({ valid: 'true' });
+    res.status(200).json({ valid: "true" });
   });
 });
 
 router.get("/logout", (req, res) => {
-  res.clearCookie('token');
-  res.status(200).json({ message: 'Logout realizado com sucesso' });
+  res.clearCookie("token");
+  res.status(200).json({ message: "Logout realizado com sucesso" });
 });
 
 module.exports = router;
-
-
-
